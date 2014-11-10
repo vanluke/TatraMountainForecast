4ae9d0b6a91e02d3fa6a48d7cc217295de9ba338
Lukasz Gonciarz <lgonciarz@infusion.com>
2014-11-10 11:04:22 +0100

    init

---
+++ TatraMountainForecast/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Optimization;
+
+namespace App_Start
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                       "~/Scripts/jquery-{version}.js"
+                 ));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css"
+                ));
+        }
+    }
+}
