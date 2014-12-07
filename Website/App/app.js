269dfc81ea521f17bb7762d8981696793b561648
vanluke22 <vanluke@op.pl>
2014-12-07 18:52:31 +0100

    n

---
+++ Website/App/app.js
@@ -0,0 +1,10 @@
+﻿define(['angularAMD', 'angular-route', 'angular-animate', 'angular-locale', 'jquery', 'util', 'moment', 'moment-pt-br'],
+    function (angularAMD, route, animate, locale, $, util) {
+        var app = angular.module("app", ['ngRoute', 'ngAnimate']);
+        app.config(function($routeProvider) {
+            $routeProvider.when("/weathers", angularAMD.route({
+                templateUrl: '../../Weathers', controller: 'WeathersController', section: 'Weathers', title: 'Weathers'
+            })
+            .otherwise({ redirectTo: '/Home' })
+        });
+    });
---
+++ Website/App/main.js
@@ -0,0 +1,20 @@
+﻿require.config({
+    baseUrl: "app",
+    // alias libraries paths
+    paths: {
+        'angular': './../Scripts/angular',
+        'angular-locale': './../Scripts/i18n/angular-locale_pt-br',
+        'angular-route': './../Scripts/angular-route',
+        'angular-animate': './../Scripts/angular-animate',
+        'async': './../Scripts/async',
+        'angularAMD': './../Scripts/angularAMD',
+        'ngload': './../Scripts/ngload',
+        'jquery': './../Scripts/jquery-1.10.2.min',
+        'underscore': './../Scripts/underscore',
+        'moment': './../Scripts/moment.min',
+        'spin': './../Scripts/spin',
+        'bootstrap': './../Scripts/bootstrap',
+
+        'ngWeathersController': 'controllers/ngWeathersController'
+    },
+});
